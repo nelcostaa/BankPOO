6d09521d64b44e9d412697dde5c386b440cd832d
nelcostaa <nelcostaa@hotmail.com>
2024-11-21 23:23:39 -0300

    add poupanca data to operacoes array

--- src/Poupanca.java
+++ src/Poupanca.java
@@ -1,12 +1,19 @@
+import java.time.LocalDate;
+
 public final class Poupanca extends Conta implements Remunerada {
 
   public Poupanca(String nomeConta, int numeroConta, String nomeCorrentista,
-                  int cpfCorrentista) {
+      int cpfCorrentista) {
     super(nomeConta, numeroConta, nomeCorrentista, cpfCorrentista);
   }
 
   public void aplicarTaxaCorrecao(double taxaCorrecao) {
     double novoSaldo = this.getSaldo() * (1 + taxaCorrecao);
     this.setSaldo(novoSaldo);
+
+    Operacao taxa = new Operacao(LocalDate.now(), novoSaldo, Tipo.CORRECAO);
+
+    this.adicionarOperacao(taxa);
+
   }
 }
