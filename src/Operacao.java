460dee6e01103586d9d108f0fb3221664d5aeb57
nelcostaa <nelcostaa@hotmail.com>
2024-11-21 23:25:05 -0300

    complete operacao class with getters

--- src/Operacao.java
+++ src/Operacao.java
@@ -11,4 +11,37 @@ public class Operacao {
     this.valor = valor;
     this.tipo = tipo;
   }
+
+  public double calcularImpacto() {
+    // Determina se o valor deve ser subtraído ou somado ao saldo
+    if (this.tipo == Tipo.SAQUE || this.tipo == Tipo.PIX_OUT) {
+      return -this.valor;
+    }
+    return this.valor;
+  }
+
+  public LocalDate getData() {
+    return data;
+  }
+
+  public void setData(LocalDate data) {
+    this.data = data;
+  }
+
+  public double getValor() {
+    return valor;
+  }
+
+  public void setValor(double valor) {
+    this.valor = valor;
+  }
+
+  public Tipo getTipo() {
+    return tipo;
+  }
+
+  public void setTipo(Tipo tipo) {
+    this.tipo = tipo;
+  }
+
 }
